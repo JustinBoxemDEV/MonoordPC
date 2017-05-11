3f301459fad133b7e98bf723fc2eab78a8527e64
Bas Bierhof <x.basara@hotmail.com>
2017-05-11 11:38:23 +0200

    Reservations en reservation status tabel gemaakt en gelinked.
    Invoices en alle bijbehorende tabellen gemaakt en gelinked.

---
+++ app/Invoice.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Price;
+use App\VAT;
+use App\Reservation;
+use App\Invoice_Heading;
+
+class Invoice extends Model
+{
+    protected $fillable = [
+        'id', 'price_id', 'vat_id', 'reservation_id', 'invoice_heading_id', 'invoice_number', 'invoice_date', 'invoice_date', 'invoice_weeknumber', 'invoice_description',
+    ];
+    
+    public function Price() {
+        return $this->belongsTo(Price::class, 'price_id');
+    }
+    
+    public function VAT() {
+        return $this->belongsTo(VAT::class, 'vat_id');
+    }
+    
+    public function Reservation() {
+        return $this->belongsTo(Reservation::class, 'reservation_id');
+    }
+    
+    public function Invoice_Heading() {
+        return $this->belongsTo(Invoice_Heading::class, 'invoice_heading_id');
+    }
+    
+}
---
+++ database/migrations/2017_05_11_090856_create_invoices_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('price_id')->unsigned();
+            $table->integer('vat_id')->unsigned();
+            $table->integer('reservation_id')->unsigned();
+            $table->integer('invoice_heading_id')->unsigned();
+            $table->integer('invoice_number');
+            $table->date('invoice_date');
+            $table->integer('invoice_weeknumber');
+            $table->string('invoice_description');
+            $table->timestamps();
+        });
+        
+        Schema::table('invoices', function (Blueprint $table)
+        {
+            $table->foreign('price_id')->references('id')->on('prices');  
+            $table->foreign('vat_id')->references('id')->on('vats');
+            $table->foreign('reservation_id')->references('id')->on('reservations');
+            $table->foreign('invoice_heading_id')->references('id')->on('invoice__headings');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
