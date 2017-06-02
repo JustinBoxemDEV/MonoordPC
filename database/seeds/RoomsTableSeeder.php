5dc8d2a7b4976d9ec743629969e5cb5b4fa2eec2
Gebruiker <Gebruiker@10.115.191.243>
2017-06-02 11:18:21 +0200

    Starting seeding for room types

--- database/seeds/RoomsTableSeeder.php
+++ database/seeds/RoomsTableSeeder.php
@@ -11,6 +11,12 @@ class RoomsTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('rooms')->insert(
+                [
+                    [
+                        'room_type_id' => 1,
+                        'room_name' => 'OR1',
+                    ],
+        ]);
     }
 }
