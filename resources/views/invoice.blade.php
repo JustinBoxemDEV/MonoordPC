202f6a1ea953c12304c9812444aee52ffea7af9b
Gebruiker <Gebruiker@10.115.191.243>
2017-06-28 09:47:13 +0200

    Added demo branch and this is a test commit

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Rooster</div>
+                <div class="panel-heading">Facturen</div>
 
                 <div class="panel-body">
-                    Welkom op Ted zijn trouwdag.
+                    Hier komen alle facturen
                 </div>
             </div>
         </div>
