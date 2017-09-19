c28a9e941ce290d02653c75727a49dc05a4d76a7
JustinBoxemDEV <justinboxem@hotmail.com>
2017-09-19 11:40:42 +0200

    API Functie voor password reset door email gefixed. Verzend nu email met token voor invoer in Android omgeving.

--- API/functions_laravelNetbeans.php
+++ API/functions_laravelNetbeans.php
@@ -37,20 +37,33 @@ class user {
 		$this->connection = new DB_con();
 	}
 
-	public function setQuery($query){
-		$this->query = $query;
-	}
-
 	public function changePassword($userid, $nieuwwachtwoord, $bevestigdwachtwoord) {
-		$query = setQuery("SELECT password FROM users WHERE id = '$userid'");
+		$query = "SELECT password FROM users WHERE id = '$userid'";
 		$result = $this->connection->sql($query)->fetch_assoc();
 		echo $result['password'];
                 if ($nieuwwachtwoord === $bevestigdwachtwoord) {
 		}
 	}
         
-        public function getUserData($email) {
-            $query = setQuery("SELECT id, email, firstname, lastname FROM users WHERE email = '$email'");
+        public function sendRecoveryMail($email) {
+            $token = "";
+            $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            $codeAlphabet.= "0123456789";
+            $max = strlen($codeAlphabet); // edited
+
+            for ($i = 0; $i < 6; $i++) {
+                $token .= $codeAlphabet[random_int(0, $max - 1)];
+            }
+            $query = "UPDATE users SET remember_token='$token' WHERE email = '$email'";
+            $this->connection->sql($query);
+            $from = "From: MONKlantenService@hotmail.com";
+            $topic = "Wachtwoord reset | Muziek Organizatie Noord";
+            $message = "Geachte persoon, u heeft een mail ontvangen betreffende het resetten van uw wachtwoord. Wachtwoord reset code: $token";
+            mail($email,$topic,$message,$from);
+        }
+
+    public function getUserData($email) {
+            $query = "SELECT id, email, firstname, lastname FROM users WHERE email = '$email'";
             $result = $this->connection->sql($query)->fetch_assoc();
             echo $result['id'];
             echo $result['email'];
@@ -59,7 +72,7 @@ class user {
         }
 
         public function getUserBands($userID){
-            $query = setQuery("SELECT b.id, b.band_name FROM bands as b INNER JOIN band__user__bridges as bu ON b.id = bu.band_id INNER JOIN users as u ON u.id = bu.user_id WHERE u.id = '$userID';");
+            $query = "SELECT b.id, b.band_name FROM bands as b INNER JOIN band__user__bridges as bu ON b.id = bu.band_id INNER JOIN users as u ON u.id = bu.user_id WHERE u.id = '$userID';";
             $result = $this->connection->sql($query)->fetch_assoc();
             echo $result['id'];
         }
@@ -81,21 +94,20 @@ class tempReservation{
         }
         
     public function createTempReservation($band_id, $payment_method_id, $room_id, $temp_reservation_date){
-        $this->query = "INSERT INTO `monoord`.`temporary__reservations` (`id`, `band_id`, `payment_method_id`, `room_id`, `temp_reservation_date`, `temp_delayed`, `processed`, `created_at`, `updated_at`) VALUES (NULL, '$band_id', '$payment_method_id', '$room_id', '$temp_reservation_date', '0', '0', NULL, NULL);";
-        var_dump($this->query);
-        $this->connection->sql($this->query);
+        $query = "INSERT INTO `monoord`.`temporary__reservations` (`id`, `band_id`, `payment_method_id`, `room_id`, `temp_reservation_date`, `temp_delayed`, `processed`, `created_at`, `updated_at`) VALUES (NULL, '$band_id', '$payment_method_id', '$room_id', '$temp_reservation_date', '0', '0', NULL, NULL);";
+        $this->connection->sql($query);
     }
     
     public function getCreatedAt($temp_reservation_id){
-        $query = setQuery("SELECT created_at FROM temporary__reservations WHERE id = '$temp_reservation_id'");
+        $query = "SELECT created_at FROM temporary__reservations WHERE id = '$temp_reservation_id'";
         $result = $this->connection->sql($query)->fetch_assoc();
 	return $result['created_at'];
     }
     
     public function cancelTempReservation($temp_reservation_id, $updated_at, $created_at){
-        $this->query = "UPDATE `monoord`.`temporary__reservations` SET `temp_delayed` = '1', `updated_at` = $updated_at WHERE `temporary__reservations`.`id` = $temp_reservation_id AND $created_at >= NOW() - INTERVAL 1 DAY);";
-        var_dump($this->query);
-        $this->connection->sql($this->query);
+        $query = "UPDATE `monoord`.`temporary__reservations` SET `temp_delayed` = '1', `updated_at` = $updated_at WHERE `temporary__reservations`.`id` = $temp_reservation_id AND $created_at >= NOW() - INTERVAL 1 DAY);";
+        var_dump($query);
+        $this->connection->sql($query);
     }
 }
 
@@ -109,7 +121,7 @@ class rooms {
 	}
         
         public function getAvailableRooms(){
-            $query = setQuery("SELECT room_name FROM rooms WHERE rent_status != 1");
+            $query = "SELECT room_name FROM rooms WHERE rent_status != 1";
             $result = $this->connection->sql($query)->fetch_assoc();
             return $result['room_name'];
         }
